21d379a3875afc87f03c3eee29a83ab9451a45a7
Alex Wisch <alexjwisch@gmail.com>
2025-07-01 14:53:05 -0500

    Update checkForMultipleLetters.py
    adjusted for the best runtime: 15ms, beats 75.68%

--- checkForMultipleLetters.py
+++ checkForMultipleLetters.py
@@ -34,19 +34,15 @@ Input: word = "aaaa"
 
 Output: 4
 """
-word = "ere"
-letters = list(word)
-print(letters)
-
-output=1
-
-for i in range(len(letters)):
-    if i == 0:
-        print("skipping first letter")
-    elif letters[i] == letters[i - 1]:
-        output += 1
-    print("Current letter: " + letters[i] + str(output))
-if output == len(letters) + 1:
-    output -= 1
-
-print("Number of consecutive letters: " + str(output))
+class Solution(object):
+    def possibleStringCount(self, word):
+        letters = list(word)
+        output=1
+        for i in range(len(letters)):
+            if i == 0:
+                pass
+            elif letters[i] == letters[i - 1]:
+                output += 1
+        if output == len(letters) + 1:
+            output -= 1
+        return output
