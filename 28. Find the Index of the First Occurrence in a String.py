dde061d028182e1362299ee2c277b0c27d01bac7
Alex Wisch <alexjwisch@gmail.com>
2025-07-13 13:19:32 -0500

    Add files via upload
    Took in two strings. Converted them both into arrays. As long as the length of needle + the currently checked index in the array wasn't longer than the entire haystack, went through and verified that the current index and the next ones that would make it the length of the needle itself. If they matched entirely, returned the for loop index that was currently being checked. If they did not and no index+the next few indexes to complete the length of needle matched entirely, returned -1

---
+++ 28. Find the Index of the First Occurrence in a String.py
@@ -0,0 +1,32 @@
+"""Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+ 
+
+Example 1:
+
+Input: haystack = "sadbutsad", needle = "sad"
+Output: 0
+Explanation: "sad" occurs at index 0 and 6.
+The first occurrence is at index 0, so we return 0.
+Example 2:
+
+Input: haystack = "leetcode", needle = "leeto"
+Output: -1
+Explanation: "leeto" did not occur in "leetcode", so we return -1.
+"""
+class Solution(object):
+    def strStr(self, haystack, needle):
+        h = list(haystack)
+        n = list(needle)
+
+        for i in range(len(h)):
+            while i + len(n) <= len(h):
+                if h[i:i + len(n)] == n:
+                    return(i)
+                    break
+                i += 1
+            else:
+                continue
+            break
+        else:
+            return(-1)
