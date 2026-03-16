83418e9d0d3cf202c73495d176f4456900477ad2
awisch <alexjwisch@gmail.com>
2026-03-16 11:09:04 -0500

    This one was a little tough to understand - never really heard of an ugly number. Had a solution that essentially just divided the numbers once, but ran into 8 which can be divided by 2 three times to end up with 1, which would make that an ugly number as well. Because of this, had to implement a while loop to keep dividing by either 2 3 or 5 to see if the modulus was 0 meaning it was dividing evenly. Eventually at the end, you want to get to 1 which also covers the edge case of starting with 1. Had to also add a if n == 0 statement in the while loop (I guess I couldve also added this to the top of the function as well) but whatever...completed!
    Runtime - 0ms: Beats 100%
    Memory - 12.34mb: Beats 79.30%

---
+++ 263. Ugly Number.py
@@ -0,0 +1,40 @@
+"""An ugly number is a positive integer which does not have a prime factor other than 2, 3, and 5.
+
+Given an integer n, return true if n is an ugly number.
+
+ 
+
+Example 1:
+
+Input: n = 6
+Output: true
+Explanation: 6 = 2 × 3
+Example 2:
+
+Input: n = 1
+Output: true
+Explanation: 1 has no prime factors.
+Example 3:
+
+Input: n = 14
+Output: false
+Explanation: 14 is not ugly since it includes the prime factor 7.
+"""
+n = 14
+def isUgly(self, n):
+    while n % 2 == 0 or n % 3 == 0 or n % 5 == 0:
+        if n % 2 == 0:
+            print("2 is a factor")
+            n = n / 2
+        if n % 3 == 0:
+            print("3 is a factor")
+            n = n / 3
+        if n % 5 == 0:
+            print("5 is a factor")
+            n = n / 5
+        if n == 0:
+            return False
+    if n == 1:
+        return True
+    else:
+        return False
